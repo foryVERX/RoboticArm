4ea4af9c45a8d59aab87887a4dcde9160d0f7f6b
foryVERX <59301678+foryVERX@users.noreply.github.com>
2021-10-11 08:57:33 +0300

    Yousef - Uploaded
    Motors Calibration Codes using python and ino.

---
+++ Calibration_test.py
@@ -0,0 +1,40 @@
+from pyfirmata import Arduino, SERVO, util
+from time import sleep
+from serial import Serial
+
+port = 'COM3'
+pin = 9
+board = Arduino(port)
+board.digital[pin].mode = SERVO
+pin9 = board.get_pin('d:9:s')
+
+
+# First method to move the servo
+def rotate_servo(pin, angle):
+    board.digital[pin].write(angle)
+
+
+# Second method to move the servo
+def move_servo(angle):
+    pin9.write(angle)
+
+
+rotate_servo(pin, 90)
+
+while True:
+    # Reads and handles data from the microcontroller over the serial port. This method
+    # should be called in a
+    # main loop or in an Iterator instance to keep this boards pin values up to date
+    iter8 = util.Iterator(board)
+    iter8.start()
+    sleep(0.15)
+    user_angle = input("Choose an angle =  ")
+    try:  # To only accept integer values
+        int(user_angle)
+        its_int = True
+    except ValueError:
+        its_int = False
+    if its_int:
+        rotate_servo(pin, user_angle)
+        sleep(0.15)
+
