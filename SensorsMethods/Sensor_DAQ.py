4855762b220c83f0777b8baea3d62522611aef80
Ahmed <ahmadbagasi22@gmail.com>
2021-10-14 19:03:40 +0300

    com change

--- SensorsMethods/Sensor_DAQ.py
+++ SensorsMethods/Sensor_DAQ.py
@@ -4,21 +4,26 @@ import time
 ser = serial.Serial('COM4', baudrate=9600, timeout=1)
 
 
-def getValues():
+def get_values():
     ser.write(b'g')
-    arduinoData = ser.readline().decode('ascii')
+    arduinoData = ser.readline().decode('utf').rstrip('\n')
     return arduinoData
+
 
 def get_ir_data():
     ir_data = []
     for i in range(10):
         time.sleep(0.1)
-        value = getValues()
-        if value == 0:
-            value = 1
-        else:
+        value = get_values()
+        value = [int(i) for i in value.split() if i.isdigit()]
+        for i in value:
+            value = value[0]
+        if value == 1:
             value = 0
+        else:
+            value = 1
         ir_data.append(value)
     return ir_data[9]
 
-get_ir_data()
+print(get_ir_data())
+
