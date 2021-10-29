a4567129488f7ac2032855988a5ec65cfef4ca46
foryVERX <verrrx@gmail.com>
2021-10-29 10:11:02 +0300

    added new code

--- MainProgram/easyEEZYbotARM-master/examples/example1_plot.py
+++ MainProgram/easyEEZYbotARM-master/examples/example1_plot.py
@@ -4,15 +4,17 @@ from easyEEZYbotARM.kinematic_model import EEZYbotARM_Mk1
 
 # initial joint angles
 jointAngle1 = 0
-jointAngle2 = 70
-jointAngle3 = -100
+jointAngle2 = 90
+jointAngle3 = -135
 
 # Create an EEZYbotARM Mk2 object
 myRobotArm = EEZYbotARM_Mk2(jointAngle1, jointAngle2, jointAngle3)
 # Plot it
-myRobotArm.plot()
 
-# Create an EEZYbotARM Mk1 object
-myRobotArm = EEZYbotARM_Mk1(jointAngle1, jointAngle2, jointAngle3)
-# Plot it
+
+servoAngle_q1, servoAngle_q2, servoAngle_q3 = myRobotArm.map_kinematicsToServoAngles()
+print("Base Servo Angle Q1= ", servoAngle_q1)
+print("Main arm Servo Angle Q2= ", servoAngle_q2)
+print("Hor arm Servo Angle Q3= ", servoAngle_q1)
+
 myRobotArm.plot()
