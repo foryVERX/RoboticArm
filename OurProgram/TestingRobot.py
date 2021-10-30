9b93de8e5b69e8a20d63e700ef5a524368210aa7
foryVERX <verrrx@gmail.com>
2021-10-30 12:47:51 +0300

    Organized the program and created methods for robot

--- OurProgram/TestingRobot.py
+++ OurProgram/TestingRobot.py
@@ -1,18 +1,62 @@
 from OurProgram.RobotClass import *
+import keyboard
 
 MyRoboticArm = RoboticArm()
-MyRoboticArm.SuddenMovementCorrection()
 
-while True:
+runCount = 0
+x_value = 190
+y_value = 0
+z_value = 120
+ee_value = 100
+
+
+def RunXYZ(x, y, z):
     speed = 3000
-    x, y, z, EE_Angle, command = input("Enter X Y Z, EE_Angle, command(stop)).split()").split()
     x = int(x)
     y = int(y)
     z = int(z)
-    EE_Angle = int(EE_Angle)
-    command = str(command)
     MyRoboticArm.GoToXyZ(x, y, z, speed)
-    MyRoboticArm.EndEffector(EE_Angle, speed)
-    if command == "stop":
-        MyRoboticArm.GoToMainHomePosition(speed)
-        break
+
+
+def RunEE(angle, speed):
+    MyRoboticArm.EndEffector(angle, speed)
+
+
+def on_press_reaction(event):
+    global x_value, y_value, z_value, ee_value
+    if event.name == 'q':
+        x_value += 1
+    if event.name == 'w':
+        y_value += 1
+    if event.name == 'e':
+        z_value += 1
+    if event.name == 'a':
+        x_value -= 1
+    if event.name == 's':
+        y_value -= 1
+    if event.name == 'd':
+        z_value -= 1
+    if event.name == 'enter':
+        RunXYZ(x_value, y_value, z_value)
+    if event.name == 'o':
+        ee_value = 100
+        speed = 3000
+        RunEE(ee_value, speed)
+    if event.name == 'c':
+        ee_value = 60
+        speed = 3000
+        RunEE(ee_value, speed)
+
+    print("\n\nx_value = ", x_value)
+    print("y_value = ", y_value)
+    print("z_value = ", z_value)
+    print("ee_value = ", ee_value)
+
+
+keyboard.on_press(on_press_reaction)
+
+while True:
+    if runCount == 0:
+        MyRoboticArm.SuddenMovementCorrection()
+        runCount += 1
+    pass
